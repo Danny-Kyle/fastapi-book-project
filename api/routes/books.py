956a093708b5b590e932d54c566a869c2bca9fee
Okechukwu Daniel <88836879+Danny-Kyle@users.noreply.github.com>
2025-02-10 16:19:26 +0000

    Made changes to books.py and added missing endpoint GET book_by_id

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,5 +1,6 @@
 from typing import OrderedDict
 
+from fastapi import HTTPException
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 
@@ -55,6 +56,12 @@ async def update_book(book_id: int, book: Book) -> Book:
         content=db.update_book(book_id, book).model_dump(),
     )
 
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book_by_id(book_id: int):
+    book = db.get_books().get(book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail= "Book not Found")
+    return book
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int) -> None:
